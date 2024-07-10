eb4d2e071133a8b5be48bb2ac64cd5427ee2d34d
martyna.kiljan <martyna.kiljan@nsgroup.ch>
2024-07-10 14:04:19 +0200

    some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import img1 from "../assets/header-images/header-img-1.jpg";
 import img2 from "../assets/header-images/header-img-2.jpg";
 import img3 from "../assets/header-images/header-img-3.jpg";
+import img12 from "../assets/header-images/header-img-12.png";
 import { useTranslation } from 'react-i18next';
 
 
@@ -21,9 +22,7 @@ const Header = () => {
   return (
     <header className="header section" id="home">
       <div className="header__inner">
-        <img className="header__img header__img-1 kenburns-top" src={img1} alt="wedding" />
-        <img className="header__img header__img-2" src={img2} alt="couple" />
-        <img className="header__img header__img-3" src={img3} alt="couple" />
+        <img className="header__img header__img-1 kenburns-top" src={img12} alt="wedding" />
       </div>
       <div className="header__bottom">
         <p className="header__text">
