11cf222737f9a5a13aab12dec9185be8b56eb92d
martyna.kiljan <martyna.kiljan@nsgroup.ch>
2024-07-11 10:19:25 +0200

    compress img main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,38 @@
-import React, { useEffect } from "react";
-import img12 from "../assets/header-images/header-img-12.png";
-import { useTranslation } from 'react-i18next';
+import React, { useEffect } from 'react'
+import img from '../assets/header-images/header-img-main.png'
+import { useTranslation } from 'react-i18next'
 
 const Header = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation()
 
   useEffect(() => {
-    const images = document.querySelectorAll(".header__img");
+    const images = document.querySelectorAll('.header__img')
 
-    images.forEach(img => {
+    images.forEach((img) => {
       img.onload = () => {
-        img.classList.add("animate-opacity");
-      };
-    });
-  }, []);
+        img.classList.add('animate-opacity')
+      }
+    })
+  }, [])
 
   return (
     <header className="header section" id="home">
       <div className="header__inner">
-        <img className="header__img header__img-1 kenburns-top" src={img12} alt="wedding" />
+        <img
+          className="header__img header__img-1 kenburns-top"
+          src={img}
+          alt="wedding"
+        />
       </div>
       <div className="header__bottom">
-        <p className="header__text">
-       {t('header.title')}
-        </p>
-        <a className="header__button"  href="#contact" >  {t('header.buttonText')}</a>
+        <p className="header__text">{t('header.title')}</p>
+        <a className="header__button" href="#contact">
+          {' '}
+          {t('header.buttonText')}
+        </a>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
