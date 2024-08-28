105c0200a547018e987f0b01f8b84a428ecdb2e3
Martyna Kiljan <kmartynak@icloud.com>
2024-08-28 14:37:12 +0200

    app.js update

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,49 @@
-import "./styles/all.scss";
-import Menu from "./components/Menu";
-import Header from "./components/Header";
-import AboutMe from "./components/AboutMe";
-import Services from "./components/Services";
-import Process from "./components/Process";
-import Instagram from "./components/Instagram";
-import Footer from "./components/Footer";
-import Contact from "./components/Contact";
-import Projects from "./components/Projects";
-import Spinner from "./components/Spinner";
-import React, {useState, useEffect} from "react";
-import LanguageSwitcher from "./components/TopButtons";
-import ScrollToTopButton from "./components/ScrollToTopButton";
-
+import './styles/all.scss'
+import Menu from './components/Menu'
+import Header from './components/Header'
+import AboutMe from './components/AboutMe'
+import Services from './components/Services'
+import Process from './components/Process'
+import Instagram from './components/Instagram'
+import Footer from './components/Footer'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
+import Spinner from './components/Spinner'
+import React, { useState, useEffect } from 'react'
+import LanguageSwitcher from './components/TopButtons'
+import ScrollToTopButton from './components/ScrollToTopButton'
 
 function App() {
-  const [loading, setLoading] = useState(true);
+	const [loading, setLoading] = useState(true)
 
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setLoading(false)
+		}, 500)
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false); 
-    }, 1000);
+		return () => clearTimeout(timer)
+	}, [])
 
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <div className="App">
-      {loading ? (
-        <Spinner /> 
-      ) : (
-        <div className="wrapper">
-          <LanguageSwitcher/>
-          <Menu />
-          <Header />
-          <AboutMe />
-          <Services />
-          <Process />
-          <Projects />
-          <Instagram />
-          <Contact />
-          <ScrollToTopButton/>
-          <Footer />
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className='App'>
+			{loading ? (
+				<Spinner />
+			) : (
+				<div className='wrapper'>
+					<LanguageSwitcher />
+					<Menu />
+					<Header />
+					<AboutMe />
+					<Services />
+					<Process />
+					<Projects />
+					<Instagram />
+					<Contact />
+					<ScrollToTopButton />
+					<Footer />
+				</div>
+			)}
+		</div>
+	)
 }
-export default App;
+export default App
